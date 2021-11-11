bfd6f0ba8f75473f74f6a4b9c335b75b0fede0bf
Vanessa Scharff <vanessa.scharff97@gmail.com>
2021-11-11 10:44:06 +0100

    uebung6

--- Uebung5/uebung5/lib/main.dart
+++ Uebung5/uebung5/lib/main.dart
@@ -1,31 +1,4 @@
-// ignore_for_file: avoid_print
-
-/* 1. Hardcoded
-
-- Guck auf Terminal
-- Guck auf Firebase dabei
-- Click Read = null
-- Click Create -> Read = John Peter
-- Click Update -> Read = Alan Peter
-- Click Delete -> Read = null
-
-
-2. Ohne Hardcoded Key
-
-Code: 
-- Add 'CreateNew'
-- Add 'ReadAll'
-Terminal:
-- Guck auf Firebase dabei
-1 Click Read
-2 Click CreateNew 1x -> ReadAll -> 1 Eintrag Mehr
-3 Click CreateNew 4x -> ReadAll -> 4 Einträge Mehr
-4 Lösche Einträge in Firebase -> Read = Einträge weg, nicecreme.
-
-.. löschen modifiyieren etc
-
-*/
-
+// Grundlagen von https://medium.com/app-dev-community/flutter-firebase-crud-tutorial-using-new-firebase-libraries-2021-9af6a7494d66
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -71,38 +44,11 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-//Neu
-  void _createNew() async {
-    try {
-      await firestore.collection('users').doc().set({
-        'firstName': 'John',
-        'lastName': 'Peter',
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
   void _read() async {
     DocumentSnapshot documentSnapshot;
     try {
       documentSnapshot = await firestore.collection('users').doc('testUser').get();
       print(documentSnapshot.data().toString());
-    } catch (e) {
-      print(e);
-    }
-  }
-
-//neu
-  void _readAll() async {
-    QuerySnapshot querySnapshot;
-    try {
-      print('-------------- Start of _readAll ----------------');
-      querySnapshot = await firestore.collection('users').get();
-      querySnapshot.docs.forEach((doc) {
-        print(doc.data().toString());
-      });
-      print('-------------- End of _readAll ----------------');
     } catch (e) {
       print(e);
     }
@@ -150,19 +96,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: const Text("Delete"),
             onPressed: _delete,
           ),
-
-
-//neu 
-          ElevatedButton(
-            child: const Text("CreateNew"),
-            onPressed: _createNew,
-          ),
-          ElevatedButton(
-            child: const Text("ReadAll"),
-            onPressed: _readAll,
-          ),
-
-          // UserInformation()
         ]),
       ),
     );
---
+++ Uebung6/uebung6/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uebung6/flavor.dart';
+import 'package:uebung6/my_app.dart';
+
+void main() {
+  runApp(Provider<Flavor>.value(
+    value: Flavor.dev,
+    child: const MyApp(),
+    ));
+}
